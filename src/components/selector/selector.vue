300ed6f1433d661939f750f8e4af97df37724f18
戚桂旭 <qigx@MFWdeMacBook-Pro-2.local>
2018-08-23 13:06:34 +0800

    init

---
+++ src/components/selector/selector.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="selector">
+    <input :value='value' type="text" class="selector-input" />
+    <ul class="selector-list">
+      <li class="selector-list-item">123</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '123',
+    };
+  },
+  mounted() {
+    const query = wx.createSelectorQuery();
+    query.select('.selector').boundingClientRect();
+    query.exec((res) => {
+      console.log(res);
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.selector {
+  position: relative;
+  &-input {
+    border: 1px solid #ddd;
+    outline: none;
+  }
+  &-list {
+    list-style: none;
+    position: absolute;
+    top: 100%;
+    left: 0;
+  }
+}
+</style>
